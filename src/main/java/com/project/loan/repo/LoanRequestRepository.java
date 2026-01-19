afee4b1f0b0f16373bfa91661bb04ef820198993
Celiacv28 <celiacv28@gmail.com>
2026-01-19 14:17:20 +0100

    Fix Loan Request UserId

--- src/main/java/com/project/loan/repo/LoanRequestRepository.java
+++ src/main/java/com/project/loan/repo/LoanRequestRepository.java
@@ -8,14 +8,13 @@ import org.springframework.stereotype.Repository;
 
 import com.project.loan.models.LoanRequest;
 import com.project.loan.models.LoanStatus;
-import com.project.loan.models.User;
 
 @Repository
 public interface LoanRequestRepository extends JpaRepository<LoanRequest, Long> {
     
 
-    @Query("SELECT lr FROM LoanRequest lr WHERE (:status IS NULL OR lr.status = :status) AND (:user IS NULL OR lr.user = :user) AND (:currency IS NULL OR LOWER(lr.currency) = LOWER(:currency))")
+    @Query("SELECT lr FROM LoanRequest lr WHERE (:status IS NULL OR lr.status = :status) AND (:userId IS NULL OR lr.user.id = :userId) AND (:currency IS NULL OR lr.currency = :currency)")
     List<LoanRequest> findByFilters(@org.springframework.data.repository.query.Param("status") LoanStatus status,
-                                    @org.springframework.data.repository.query.Param("user") User user,
-                                    @org.springframework.data.repository.query.Param("currency") String currency);
+                                    @org.springframework.data.repository.query.Param("userId") Long userId,
+                                    @org.springframework.data.repository.query.Param("currency") String currency);   
 }
